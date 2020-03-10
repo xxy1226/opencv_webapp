1255be073dabd20f621e9ec3ed081352a35a9a11
xxy1226 <andrew.xia.mb@gmail.com>
2020-03-10 00:07:36 -0500

    Auto stash before merge of "master" and "origin/master"
    Face and eyes detection

--- opencv_webapp/opencv_webapp/forms.py
+++ opencv_webapp/opencv_webapp/forms.py
@@ -3,3 +3,10 @@ class UploadImageForm(forms.Form):
     title = forms.CharField(max_length=50)
     #file = forms.Filefield()
     image = forms.ImageField()
+    
+from .models import ImageUploadModel
+
+class ImageUploadForm(forms.ModelForm):
+    class Meta:
+        model = ImageUploadModel
+        fields = ('description','document')
--- opencv_webapp/opencv_webapp/views.py
+++ opencv_webapp/opencv_webapp/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from .forms import UploadImageForm
 from django.core.files.storage import FileSystemStorage
+from .forms import ImageUploadForm
+from django.conf import settings
+from .opencv_dface import opencv_dface
 
 def first_view(request):
     return render(request, 'opencv_webapp/first_view.html', {})
@@ -21,3 +24,18 @@ def uimage(request):
     else:
         form = UploadImageForm()
         return render(request, 'opencv_webapp/uimage.html',{'form': form})
+    
+def dface(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            
+            imageURL = settings.MEDIA_URL + form.instance.document.name
+            opencv_dface(settings.MEDIA_ROOT_URL + imageURL)
+            
+            return render(request, 'opencv_webapp/dface.html', {'form':form, 'post':post})
+    else:
+            form = ImageUploadForm()
+    return render(request, 'opencv_webapp/dface.html',{'form':form})
